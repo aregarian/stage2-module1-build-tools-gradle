fd8880a69cc22e481e6f344db0c679f536a8e2e5
fannydor <adamregarian@gmail.com>
2023-11-20 05:38:20 +0300

    asdasdasd

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,5 +4,6 @@ public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
 //        return StringUtils.isNumeric(str) && Double.parseDouble(str) > 0;
+        return true;
     }
 }
